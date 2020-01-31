4f08354330dbba94b3bae5844a67e340d6d71bc7
Éric <eric.jl1994@gmail.com>
2020-01-31 14:40:57 +0000

    Card movement 1

---
+++ SarandongaTest/Assets/Scripts/CardMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMovement : MonoBehaviour {
+
+    private Vector2 boardPosition;
+    private Vector2 offset;
+
+
+    private void OnMouseDown() {
+        boardPosition = transform.position;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag() {
+        transform.position = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+    }
+
+    private void OnMouseUp() {
+        transform.position = boardPosition;
+    }
+}
