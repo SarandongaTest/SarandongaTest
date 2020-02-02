58b8937d2d36ae4e51d3bdb2b898fa1b6276fed2
Éric <eric.jl1994@gmail.com>
2020-02-02 02:50:24 +0000

    Added JSON support

--- SarandongaTest/Assets/Scripts/Card.cs
+++ SarandongaTest/Assets/Scripts/Card.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+[Serializable]
 public class Card : ScriptableObject {
 
     public new string name;
     [TextArea]
     public string description;
+
+    public string BuildJSON() {
+        return JsonUtility.ToJson(this);
+    }
+
+    public void ExtractJSON(string json) {
+        JsonUtility.FromJsonOverwrite(json, this);
+    }
 }
